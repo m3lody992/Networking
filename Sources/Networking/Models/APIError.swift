8121794e0c84e6a5d079a8f99c410b408202aa25
rast <102649255+m3lody992@users.noreply.github.com>
2023-04-03 14:37:47 +0200

    rastarstarst

--- Sources/Networking/Models/APIError.swift
+++ Sources/Networking/Models/APIError.swift
@@ -12,17 +12,20 @@ public struct APIError: Error, Codable {
 
     public let status: String
     public let message: String?
+    public let spam: Bool?
     public let feedbackTitle: String?
     
-    public init(status: String, message: String?, feedbackTitle: String?) {
+    public init(status: String, message: String?, spam: Bool?, feedbackTitle: String?) {
         self.status = status
         self.message = message
+        self.spam = spam
         self.feedbackTitle = feedbackTitle
     }
 
     enum CodingKeys: String, CodingKey {
         case status
         case message
+        case spam
         case feedbackTitle = "feedback_title"
     }
 
