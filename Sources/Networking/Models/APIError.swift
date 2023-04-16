7711be68e2f8f76f7cd778f613f647739c3088b8
rast <102649255+m3lody992@users.noreply.github.com>
2023-04-16 21:34:23 +0200

    arstyemt

--- Sources/Networking/Models/APIError.swift
+++ Sources/Networking/Models/APIError.swift
@@ -15,13 +15,15 @@ public struct APIError: Error, Codable {
     public let message: String?
     public let spam: Bool?
     public let feedbackTitle: String?
+    public let code: Int?
     
-    public init(status: String, requireLogin: Bool?, message: String?, spam: Bool?, feedbackTitle: String?) {
+    public init(status: String, requireLogin: Bool?, message: String?, spam: Bool?, feedbackTitle: String?, code: Int? = nil) {
         self.status = status
         self.requireLogin = requireLogin
         self.message = message
         self.spam = spam
         self.feedbackTitle = feedbackTitle
+        self.code = code
     }
 
     enum CodingKeys: String, CodingKey {
@@ -30,6 +32,7 @@ public struct APIError: Error, Codable {
         case message
         case spam
         case feedbackTitle = "feedback_title"
+        case code
     }
 
 }
